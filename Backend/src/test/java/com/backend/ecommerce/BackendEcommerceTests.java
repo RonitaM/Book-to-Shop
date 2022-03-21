711200feb0ec38fd2aa419fd9aa6633d31844f1d
Ronita Mitra <86037961+RonitaM@users.noreply.github.com>
2022-03-21 23:02:27 +0530

    Update BackendEcommerceTests.java

--- Backend/src/test/java/com/backend/ecommerce/BackendEcommerceTests.java
+++ Backend/src/test/java/com/backend/ecommerce/BackendEcommerceTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class YoutubeEcommerceAppApplicationTests {
+public class BackendEcommerceTests {
 
 	@Test
 	public void contextLoads() {
